4ac0e881d3e20e4181008a5a35f033275df23ccb
esperanzoski <esperanza.alvarado.v@gmail.com>
2026-03-26 10:44:20 -0300

    minimos

--- cachipun_pro.py
+++ cachipun_pro.py
@@ -1,6 +1,6 @@
 from random import randint 
 
-posibilidades = ['Rock', 'Paper', 'Scissors', 'Lizard', 'Spork']
+posibilidades = ['Rock', 'Paper', 'Scissors', 'Lizard', 'Spok']
 
 contador_compu = 0
 contador_usuario = 0
@@ -8,3 +8,7 @@ contador_usuario = 0
 while contador_compu < 3 and contador_usuario < 3:
     if contador_usuario >= 3 or contador_compu >= 3:
         break
+    
+    mov_compu = randint(0,4)
+    jugada_compu = posibilidades[mov_compu]
+    usuario = input('Rock, Paper, Scissors, Lizard or Spok... ')
