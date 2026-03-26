b26e265cb8d6fd5650b66f34b360fcef237d9204
esperanzoski <esperanza.alvarado.v@gmail.com>
2026-03-26 10:41:58 -0300

    condiciones while

--- cachipun_pro.py
+++ cachipun_pro.py
@@ -5,3 +5,6 @@ posibilidades = ['Rock', 'Paper', 'Scissors', 'Lizard', 'Spork']
 contador_compu = 0
 contador_usuario = 0
 
+while contador_compu < 3 and contador_usuario < 3:
+    if contador_usuario >= 3 or contador_compu >= 3:
+        break
